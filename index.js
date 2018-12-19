f5720f96fb567ee0bc0a7047316f3fe32d96784e
mhartington <mikehartington@gmail.com>
2018-12-19 17:14:04 -0500

    bump

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-module.exports = {
-  name: {
-    first: 'Michael',
-    middle: 'Robert',
-    last: 'Hartington',
-  },
-  birthday: '1990-02-19',
-  twitter: 'https://twitter.com/mhartington',
-  github: 'https://github.com/mhartington',
-  website: 'http://mhartington.io'
-}
+#!/usr/bin/env node
+const {white, cyan, green,bold, blue} = require("colorette")
+
+// Text + chalk definitions
+var work = white(`${blue('Ionic')}, Developer, Advocate`)
+var twitter = cyan('https://twitter.com/mhartington')
+var github = cyan('https://github.com/mhartington')
+var site = cyan('https://mhartington.io')
+var npx = white('npx mhartington')
+
+var newline = '\n'
+var heading = `${white('  Mike Hartington /')} ${cyan('mhartington')}`
+var working = `${white(bold('     Work:'))}  ${work}`
+var twittering = `${white(bold('  Twitter:'))}  ${twitter}`
+var githubing = `${white(bold('   GitHub:'))}  ${github}`
+var webing = `${white(bold('      Web:'))}  ${site}`
+var carding = `${white(bold('     Card:'))}  ${npx}`
+
+let output = heading + newline + newline + working  + newline + twittering + newline + githubing + newline + webing  + newline  + newline + carding
+
+
+console.log(output)
