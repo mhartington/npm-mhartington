3f2290eb6c22feb749fce80c7404aca9d40dc4a4
Mike Hartington <mikehartington@gmail.com>
2024-08-14 11:05:48 -0400

    update

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 #!/usr/bin/env node
-const {white, cyan, green,bold, blue} = require("colorette")
+import {white, cyan, bold, blue} from 'colorette'
 
 // Text + chalk definitions
-var work = white(`${blue('Ionic')}, Developer, Advocate`)
-var twitter = cyan('https://twitter.com/mhartington')
-var github = cyan('https://github.com/mhartington')
-var site = cyan('https://mhartington.io')
-var npx = white('npx mhartington')
+const work = white(`${blue('Nx DevTools')}, Developer, Advocate`)
+const twitter = cyan('https://twitter.com/mhartington')
+const github = cyan('https://github.com/mhartington')
+const site = cyan('https://mhartington.io')
+const npx = white('npx mhartington')
 
-var newline = '\n'
-var heading = `${white('  Mike Hartington /')} ${cyan('mhartington')}`
-var working = `${white(bold('     Work:'))}  ${work}`
-var twittering = `${white(bold('  Twitter:'))}  ${twitter}`
-var githubing = `${white(bold('   GitHub:'))}  ${github}`
-var webing = `${white(bold('      Web:'))}  ${site}`
-var carding = `${white(bold('     Card:'))}  ${npx}`
+const newline = '\n'
+const heading = `${white('  Mike Hartington |')} ${cyan('mhartington')}`
+const working = `${white(bold('     Work:'))}  ${work}`
+const twittering = `${white(bold('  Twitter:'))}  ${twitter}`
+const githubing = `${white(bold('   GitHub:'))}  ${github}`
+const webing = `${white(bold('      Web:'))}  ${site}`
+const carding = `${white(bold('     Card:'))}  ${npx}`
 
-let output = heading + newline + newline + working  + newline + twittering + newline + githubing + newline + webing  + newline  + newline + carding
+const output = heading + newline + newline + working  + newline + twittering + newline + githubing + newline + webing  + newline  + newline + carding
 
 
 console.log(output)
